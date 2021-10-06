31d63888d436899745a3acde8951b2a1ebce1546
Ashik Rahman <ashrhmn99@gmail.com>
2021-10-06 15:42:42 +0600

    Add multiple user login/signup

--- login/logincheck.php
+++ login/logincheck.php
@@ -1,24 +1,45 @@
 <?php
-	session_start();
+session_start();
 
-	if(isset($_POST['submit'])){
-		$username = $_POST['username'];
-		$password = $_POST['password'];
+function isCredCorrect($username, $password)
+{
+	$users = $_SESSION['users'];
+	$numOfUsers = count($users);
 
-		if($username != ""){
-			if($password != ""){
-							
-				if($_SESSION['user']['username'] == $username && $_SESSION['user']['password'] == $password){
-					$_SESSION['flag'] = "true";
-					header('location: home.php');
-				}else{
-					echo "invalid username/password";
-				}
-			}else{
-				echo "Invalid password...";
-			}
-		}else{
-			echo "Invalid username...";
+	for ($i = 0; $i < $numOfUsers; ++$i) {
+		if ($users[$i]['username'] == $username && $users[$i]['password'] == $password) {
+			echo 'here';
+			return true;
 		}
+		echo 'there';
+		echo $users[$i].'    '.$username.'   '.$password;
 	}
-?>
+	return false;
+}
+
+if (isset($_POST['submit'])) {
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+
+	if ($username != "") {
+		if ($password != "") {
+
+			if (isCredCorrect($username, $password)) {
+				$_SESSION['flag'] = "true";
+				header('location: home.php');
+			} else {
+				echo "invalid username/password";
+			}
+			// if($_SESSION['user']['username'] == $username && $_SESSION['user']['password'] == $password){
+			// 	$_SESSION['flag'] = "true";
+			// 	header('location: home.php');
+			// }else{
+			// 	echo "invalid username/password";
+			// }
+		} else {
+			echo "Invalid password...";
+		}
+	} else {
+		echo "Invalid username...";
+	}
+}
--- login/signupcheck.php
+++ login/signupcheck.php
@@ -8,8 +8,16 @@
 
 					//$_SESSION['username'] 	= $_POST['username'];
 					//$_SESSION['password'] 	= $_POST['password'];
+					$users = $_SESSION['users'];
+					if(!$users){
+						$users = array();
+						$_SESSION['users'] = $users;
+					}
 					$user = ['username'=> $_POST['username'], 'password'=> $_POST['password']];
-					$_SESSION['user'] = $user;
+					array_push($users,$user);
+					
+					$_SESSION['users'] = $users;
+					// $_SESSION['user'] = $user;
 					//print_r($_SESSION);
 					header('location: login.html');
 				}else{
