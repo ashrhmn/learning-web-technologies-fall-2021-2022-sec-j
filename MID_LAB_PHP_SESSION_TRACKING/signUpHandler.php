0348dc3bbae04646e895da3f8471bcea8ab657d1
Ashik Rahman <ashrhmn99@gmail.com>
2021-10-11 18:12:10 +0600

    Add check for confirmPasswords in signup

--- MID_LAB_PHP_SESSION_TRACKING/signUpHandler.php
+++ MID_LAB_PHP_SESSION_TRACKING/signUpHandler.php
@@ -6,35 +6,39 @@ if (isset($_POST['submit'])) {
     if ($_POST['username'] != "") {
         if ($_POST['password'] != "") {
             if ($_POST['email'] != "") {
+                if ($_POST['password'] == "confirmPassword") {
 
-                $users = [];
-                if (!isset($_COOKIE['users'])) {
+                    $users = [];
+                    if (!isset($_COOKIE['users'])) {
+                        setcookie('users', json_encode($users), time() + 3600, '/');
+                    } else {
+                        $users = json_decode($_COOKIE['users'], true);
+                    }
+
+                    $name = '';
+
+                    if (isset($_POST['name'])) {
+                        $name = $_POST['name'];
+                    }
+
+
+                    print_r($users);
+
+                    $user = [
+                        'username' => $_POST['username'],
+                        'password' => $_POST['password'],
+                        'email' => $_POST['email'],
+                        'name' => $_POST['name']
+                    ];
+
+                    array_push($users, $user);
+
                     setcookie('users', json_encode($users), time() + 3600, '/');
+
+                    header('location: login.php');
                 } else {
-                    $users = json_decode($_COOKIE['users'], true);
+                    echo "Passwords do not match...";
                 }
-
-                $name = '';
-
-                if (isset($_POST['name'])) {
-                    $name = $_POST['name'];
-                }
-
-
-                print_r($users);
-
-                $user = [
-                    'username' => $_POST['username'],
-                    'password' => $_POST['password'],
-                    'email' => $_POST['email'],
-                    'name' => $_POST['name']
-                ];
-
-                array_push($users, $user);
-
-                setcookie('users', json_encode($users), time() + 3600, '/');
-
-                header('location: login.php');
             } else {
                 echo "Invalid email...";
             }
