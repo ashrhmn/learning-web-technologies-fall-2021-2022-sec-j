7b88d5a235bf68785d5a700e2c6bc87110275c17
Ashik Rahman <ashrhmn99@gmail.com>
2021-10-11 15:58:30 +0600

    Changed home to index

--- MID_LAB_PHP_SESSION_TRACKING/index.php
+++ MID_LAB_PHP_SESSION_TRACKING/index.php
@@ -1,15 +1,18 @@
-<?php 
-session_start(); 
-if(isset($_COOKIE['isLoggedIn'])){ 
-    if($_COOKIE['isLoggedIn']=='true'){ 
-        header('location: dashboard.php'); 
-    } 
-    else{ 
-        header('location: login.php'); 
-    } 
-} 
-else{ 
-    setcookie('isLoggedIn','false', time()+999999,'/'); 
-    header('location: home.php'); 
-}
-?>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+
+<body>
+    <a href="dashboard.php">Dashboard</a>
+    <a href="login.php">Login</a>
+    <a href="signup.php">Signup</a>
+    Home HTML
+    X Company
+</body>
+
+</html>
