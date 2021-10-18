dad0970dc5eadb7866214ab9358587db274fec39
Ashik Rahman <ashrhmn99@gmail.com>
2021-10-18 16:54:29 +0600

    complete change password

--- MID_LAB_EXAM/changePassword.php
+++ MID_LAB_EXAM/changePassword.php
@@ -5,7 +5,17 @@ if (isset($_COOKIE['isLoggedIn'])) {
 ?>
         <form action="handleChangePassword.php" method="post">
 
+            <label for="current">Current Password</label>
+            <input type="text" name="current">
+            <label for="new">New Password</label>
+            <input type="text" name="new">
+            <label for="confirm">Retype New Password</label>
+            <input type="text" name="confirm">
+
+            <input type="submit" name="submit" value="Change">
+
         </form>
+        <a href="home.php">Go Home</a>
 <?php
     } else {
         header('location: login.php');
