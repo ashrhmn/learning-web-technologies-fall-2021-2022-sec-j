b30dc87cd761cdf393a52017b759b4ac63276de9
Ashik Rahman <ashrhmn99@gmail.com>
2021-10-18 16:57:32 +0600

    Fix empty line problem

--- MID_LAB_EXAM/handleChangePassword.php
+++ MID_LAB_EXAM/handleChangePassword.php
@@ -18,14 +18,16 @@ if (isset($_POST['submit'])) {
 
                 while (!feof($myfile)) {
                     $data = fgets($myfile);
-                    $user = explode('|', $data);
-
-                    // echo $user[0].$user[1].$user[2].$user[3];
-                    if (trim($user[0]) == $id && trim($user[1]) == $current) {
-                        $validInfo = true;
-                        array_push($allUser, trim($user[0]) . "|" . $new . "|" . trim($user[2]) . "|" . trim($user[3]) . "\r\n");
-                    } else {
-                        array_push($allUser, trim($user[0]) . "|" . trim($user[1]) . "|" . trim($user[2]) . "|" . trim($user[3]) . "\r\n");
+                    if($data!=""){
+                        $user = explode('|', $data);
+    
+                        // echo $user[0].$user[1].$user[2].$user[3];
+                        if (trim($user[0]) == $id && trim($user[1]) == $current) {
+                            $validInfo = true;
+                            array_push($allUser, trim($user[0]) . "|" . $new . "|" . trim($user[2]) . "|" . trim($user[3]) . "\r\n");
+                        } else {
+                            array_push($allUser, trim($user[0]) . "|" . trim($user[1]) . "|" . trim($user[2]) . "|" . trim($user[3]) . "\r\n");
+                        }
                     }
                 }
                 fclose($myfile);
