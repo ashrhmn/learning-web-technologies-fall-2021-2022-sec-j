c07585f1b9a6cc9f3d1c13b63f14199611dd8d83
Ashik Rahman <ashrhmn99@gmail.com>
2021-11-22 15:21:18 +0600

    Add ajax

--- ajax/abc.php
+++ ajax/abc.php
@@ -1,5 +1,19 @@
-<?php 
+<?php
 
-	echo "response from server: ".$_REQUEST['name'];
+mysqli_report(MYSQLI_REPORT_STRICT | MYSQLI_REPORT_ALL);
+$name = $_REQUEST['name'];
 
-?>
+echo $name;
+$db = new mysqli('ashrhmn.com', 'webtech', 'webtech', 'webtech');
+//
+if ($db->connect_error) {
+	echo 'Conn error';
+	return;
+}
+print_r($db);
+echo 'Conn succ';
+$db->query("select * from users");
+echo 'hh';
+$res = $db->query("select name from users")->fetch_all(MYSQLI_ASSOC);
+print_r($res);
+
